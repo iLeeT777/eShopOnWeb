cc97d022aba9c9575a6d4f439c9907b829a4a58f
BUDAPEST\Emanuil_Boyanov <emanuil_boyanov@epam.com>
2021-08-01 22:01:29 +0300

    Added retry mechanism + firing a custom event grid event when the order cannot be saved in blob storage.

--- src/AzureFunctions/OrderItemsReserverQueueConsumer.cs
+++ src/AzureFunctions/OrderItemsReserverQueueConsumer.cs
@@ -1,55 +1,89 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
 using Microsoft.Extensions.Configuration;
 using System.Text;
+using Polly;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using System.Collections.Generic;
 
 namespace AzureFunctions
 {
     public static class OrderItemsReserverQueueConsumer
     {
+        private const int RetryCount = 3;
+        private const int TimeBetweenRetries = 10; // in seconds
+
         [FunctionName("OrderItemsReserverQueueConsumer")]
-        public static async Task<IActionResult> Run(
+        public static async Task Run(
             [ServiceBusTrigger("order-items", Connection = "OrderItemReserveServiceBusUrl")] string orderItemJson, 
-            ILogger log, 
+            ILogger log,
             ExecutionContext context)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {orderItemJson}");
 
-            CloudStorageAccount storageAccount = GetCloudStorageAccount(context);
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer container = blobClient.GetContainerReference("eshop-blob-container");
+            var uploadToStorageAction = await Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(RetryCount, _ => TimeSpan.FromSeconds(TimeBetweenRetries))
+                .ExecuteAndCaptureAsync(async () =>
+                {
+                    CloudStorageAccount storageAccount = GetCloudStorageAccount(context);
+                    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                    CloudBlobContainer container = blobClient.GetContainerReference("eshop-blob-container");
 
-            await container.CreateIfNotExistsAsync();
+                    await container.CreateIfNotExistsAsync();
 
-            var fileName = $"{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss")} Order #{Guid.NewGuid():n}.json";
+                    var fileName = $"{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss")} Order #{Guid.NewGuid():n}.json";
 
-            var blob = container.GetBlockBlobReference(fileName);
-            blob.Properties.ContentType = "application/json";
+                    var blob = container.GetBlockBlobReference(fileName);
+                    blob.Properties.ContentType = "application/json";
 
-            using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(orderItemJson)))
+                    using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(orderItemJson)))
+                    {
+                        await blob.UploadFromStreamAsync(stream);
+                    }
+                });
+
+            if(uploadToStorageAction.Outcome == OutcomeType.Failure)
             {
-                await blob.UploadFromStreamAsync(stream);
-            }
+                log.LogError("Unable to store the order in blob storage.");
 
-            return new OkObjectResult("OrderItemsReserverQueueConsumer function executed successfully!");
+                var config = CreateConfig(context);
+                TopicCredentials topicCredentials = new TopicCredentials(config["EventGridAccessKey"]);
+                EventGridClient client = new EventGridClient(topicCredentials);
+
+                var eventGridEvent = new EventGridEvent()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    EventType = "AzureFunctions.OrderItems.ItemError",
+                    Data = orderItemJson,
+                    EventTime = DateTime.UtcNow,
+                    Subject = $"Unable to store the order in blob storage. Reason: {uploadToStorageAction.FinalException?.Message}",
+                    DataVersion = "1.0"
+                };
+
+                await client.PublishEventsAsync(config["EventGridTopicEndpoint"], new List<EventGridEvent> { eventGridEvent });
+            }
         }
 
         private static CloudStorageAccount GetCloudStorageAccount(ExecutionContext executionContext)
         {
-            var config = new ConfigurationBuilder()
+            var config = CreateConfig(executionContext);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(config["CloudStorageAccount"]);
+            return storageAccount;
+        }
+
+        private static IConfigurationRoot CreateConfig(ExecutionContext executionContext)
+        {
+            return new ConfigurationBuilder()
                             .SetBasePath(executionContext.FunctionAppDirectory)
                             .AddJsonFile("local.settings.json", true, true)
                             .AddEnvironmentVariables().Build();
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(config["CloudStorageAccount"]);
-            return storageAccount;
         }
     }
 }
